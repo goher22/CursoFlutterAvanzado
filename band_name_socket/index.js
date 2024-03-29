69be9f10e90b9952fecf43926df4f2c6e924740f
Carlos Gomez <gohercarlos@gmail.com>
2024-03-29 09:35:56 -0500

    Variable de entorno y Script

--- band_name_socket/index.js
+++ band_name_socket/index.js
@@ -5,10 +5,10 @@ const app = express();
 //path publico
 const publicPath = path.resolve(__dirname, "public")
 
-app.use(express.static(publicPath))
+app.use(express.static(publicPath ))
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT, (err) => {
     if(err) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', 3000);
+    console.log('Servidor corriendo en puerto', process.env.PORT);
 });
