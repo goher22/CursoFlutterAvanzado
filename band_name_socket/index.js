06d9ae5579efc4e1ac95c5f7da38caa3123d2673
Carlos Gomez <gohercarlos@gmail.com>
2024-03-29 10:20:22 -0500

    Configuracion inicial del servidor socket.io

--- band_name_socket/index.js
+++ band_name_socket/index.js
@@ -1,13 +1,30 @@
+
 const express = require("express");
 const path = require("path");
+// App de express
 const app = express(); 
+
+//Node server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Mensaje de socket
+io.on('connection', client => {
+    client.on('connect', () => {
+        console.log("Cliente conectado")
+    });
+
+    client.on('disconnect', () => {
+        console.log("Cliente desconectado")
+    });
+});
 
 //path publico
 const publicPath = path.resolve(__dirname, "public")
 
 app.use(express.static(publicPath ))
 
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
     if(err) throw new Error(err);
 
     console.log('Servidor corriendo en puerto', process.env.PORT);
