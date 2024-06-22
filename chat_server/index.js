69116bd5f976593f2e6089f58b0bac297c526589
CARLOS GOMEZ <gohercarlos@gmail.com>
2024-06-22 17:28:57 -0500

    Inicio de sesion

---
+++ chat_server/index.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const path = require("path");
+// App de express
+const app = express(); 
+
+//Node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require("./sockets/socket");
+
+
+//path publico
+const publicPath = path.resolve(__dirname, "public")
+
+app.use(express.static(publicPath ))
+
+server.listen(process.env.PORT, (err) => {
+    if(err) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+});
---
+++ chat_server/sockets/socket.js
@@ -0,0 +1,13 @@
+const { io } = require('../index');
+
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+
+});
