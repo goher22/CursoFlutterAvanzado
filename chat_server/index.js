cfac3354ee3551cb9eb201f21f4a44d950566cc5
CARLOS GOMEZ <gohercarlos@gmail.com>
2024-06-22 18:01:37 -0500

    init db config

--- chat_server/index.js
+++ chat_server/index.js
@@ -1,6 +1,11 @@
 
 const express = require("express");
 const path = require("path");
+
+//DB Config
+const {dbConnection} = require('./database/config')
+dbConnection();
+
 // App de express
 const app = express(); 
 
