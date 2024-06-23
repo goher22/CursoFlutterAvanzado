0896dc36c226a0299c15e91867c3a68e1d5616b0
CARLOS GOMEZ <gohercarlos@gmail.com>
2024-06-22 21:22:17 -0500

    conectart Node con Mongo db

--- chat_server/database/config.js
+++ chat_server/database/config.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        console.log('init db config')
+        await mongoose.connect(process.env.DB_CNN);
+        
+        console.log("DB Online")
     } catch (error) {
         console.log(error);
         throw new Error("Error en la base de datos - Hable con el admin")
