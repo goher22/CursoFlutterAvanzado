ec2d94a2e797ab077940035960fa0a7c9b846958
CARLOS GOMEZ <gohercarlos@gmail.com>
2024-08-11 09:53:58 -0500

    Cambiar el estado

--- maps_app/lib/blocs/gps/gps_bloc.dart
+++ maps_app/lib/blocs/gps/gps_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:geolocator/geolocator.dart';
@@ -6,6 +8,7 @@ part 'gps_event.dart';
 part 'gps_state.dart';
 
 class GpsBloc extends Bloc<GpsEvent, GpsState> {
+  StreamSubscription? gpsStreamSubscription;
   GpsBloc()
       : super(const GpsState(
           isGpsEnabled: false,
@@ -24,19 +27,29 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
   }
 
   Future<void> _init() async {
-    _checkGpsStatus();
+    final isEnable = await _checkGpsStatus();
+
+    add(GpsAndPermissionEvent(
+      isGpsEnable: isEnable,
+      isGpsPermissionGranted: state.isGpsPermissionGranted,
+    ));
   }
 
   Future<bool> _checkGpsStatus() async {
     final isEnable = await Geolocator.isLocationServiceEnabled();
-    Geolocator.getServiceStatusStream().listen((event) {
+    gpsStreamSubscription = Geolocator.getServiceStatusStream().listen((event) {
       final isEnable = (event.index == 1);
+      add(GpsAndPermissionEvent(
+        isGpsEnable: isEnable,
+        isGpsPermissionGranted: state.isGpsPermissionGranted,
+      ));
     });
     return isEnable;
   }
 
   @override
   Future<void> close() {
+    gpsStreamSubscription?.cancel();
     return super.close();
   }
 }
