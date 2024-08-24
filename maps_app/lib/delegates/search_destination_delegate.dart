f37a1cfbe104a386926d95024b156293a0dc4ee4
CARLOS GOMEZ <gohercarlos@gmail.com>
2024-08-24 14:38:37 -0500

    SearchDelegate - Buscador de lugares

---
+++ maps_app/lib/delegates/search_destination_delegate.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SearchDestinationDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back_ios),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text("buildResults");
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Text("buildSuggestions");
+  }
+}
---
+++ maps_app/lib/widgets/search_bar_custom.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../delegates/search_destination_delegate.dart';
+
+class SearchBarCustom extends StatelessWidget {
+  const SearchBarCustom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.only(top: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        width: double.infinity,
+        height: 50,
+        child: GestureDetector(
+          onTap: () {
+            showSearch(
+              context: context,
+              delegate: SearchDestinationDelegate(),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 13),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(100),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 5,
+                    offset: Offset(0, 5),
+                  ),
+                ]),
+            child: const Text(
+              "¿Donde quiere ir?",
+              style: TextStyle(
+                color: Colors.black87,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
