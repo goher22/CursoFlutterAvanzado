e5b140ce3687d003093028bd9f3e25c5a0a193dd
CARLOS GOMEZ <gohercarlos@gmail.com>
2024-08-25 19:06:24 -0500

    Decodificar geometry

--- maps_app/lib/blocs/search/search_bloc.dart
+++ maps_app/lib/blocs/search/search_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 
+import '../../models/route_destination.dart';
 import '../../services/traffic_service.dart';
 
 part 'search_event.dart';
@@ -23,6 +25,26 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
   Future getCootsStartToEnd(LatLng start, LatLng end) async {
     final resp = await trafficService.getCoorsStartToEnd(start, end);
 
-    return resp;
+    final geometry = resp.routes[0].geometry;
+    final distance = resp.routes[0].distance;
+    final duration = resp.routes[0].duration;
+
+    final points = decodePolyline(
+      geometry,
+      accuracyExponent: 6,
+    );
+
+    final latLngList = points
+        .map((coor) => LatLng(
+              coor[0].toDouble(),
+              coor[1].toDouble(),
+            ))
+        .toList();
+
+    return RouteDestination(
+      points: latLngList,
+      duration: duration,
+      distance: distance,
+    );
   }
 }
